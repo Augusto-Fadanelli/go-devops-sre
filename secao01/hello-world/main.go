abaf4c2d3408cda15c96a2cd50c209e980e687a4
Augusto-Fadanelli <augustofadanelli@protonmail.com>
2024-05-10 16:34:27 -0300

    Aula 7: Utilizando argumentos

--- secao01/hello-world/main.go
+++ secao01/hello-world/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main(){
-	fmt.Println("Hello World")
+	// Args é um vetor de duas posições: Args[0,1]
+	// Args[0]: Slice, nome do próprio arquivo
+	// Args[1]: Valor informado
+	if len(os.Args) !=2 { 
+		os.Exit(1) // Se for passado mais de dois argumentos, o programa será fechado.
+	}
+
+	fmt.Println("Hello", os.Args[1])
 }
