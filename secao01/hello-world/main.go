19e036d700745123e742fc2e8de55608ec068963
Augusto-Fadanelli <augustofadanelli@protonmail.com>
2024-05-10 16:34:32 -0300

    Aula 8: declaração de variáveis e funções

--- secao01/hello-world/main.go
+++ secao01/hello-world/main.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 func main(){
-	// Args é um vetor de duas posições: Args[0,1]
-	// Args[0]: Slice, nome do próprio arquivo
-	// Args[1]: Valor informado
-	if len(os.Args) !=2 { 
-		os.Exit(1) // Se for passado mais de dois argumentos, o programa será fechado.
-	}
+	name := getName()
+	idade := 23
+	fmt.Println("Hello", name, idade)
+}
 
-	fmt.Println("Hello", os.Args[1])
+func getName() string{
+	return "Augusto"
 }
