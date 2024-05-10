ed148aa7431bdf5b8888d6883fc622513a0c2c4a
Augusto-Fadanelli <augustofadanelli@protonmail.com>
2024-05-10 16:34:35 -0300

    Aula 9: funções com dois retornos

--- secao01/hello-world/main.go
+++ secao01/hello-world/main.go
@@ -5,11 +5,17 @@ import (
 )
 
 func main(){
-	name := getName()
-	idade := 23
-	fmt.Println("Hello", name, idade)
+	name, salario := "Augusto", 1000
+	setName(name)
+	newSalary, bonus := addSalary(salario, 10)
+	fmt.Println("Novo Salário:", newSalary)
+	fmt.Println("Bônus:", bonus)
 }
 
-func getName() string{
-	return "Augusto"
+func setName(name string) {
+	fmt.Println("Hello", name)
+}
+
+func addSalary(valorAtual int, bonus int) (int, int){
+	return valorAtual + bonus, bonus // Dois retornos
 }
