262ea93e5c0bb9ee60493f88bf2e7383756d6a83
Augusto-Fadanelli <augustofadanelli@protonmail.com>
2024-05-10 16:34:44 -0300

    Aula 12: arrays e slices

--- secao01/hello-world/main.go
+++ secao01/hello-world/main.go
@@ -4,26 +4,54 @@ import (
 	"fmt"
 )
 
-type Pessoa struct {
-	nome string
-	idade int
-	salario int
-}
-func (p *Pessoa) addSalary(bonus int){
-	p.salario += bonus
-}
-
 func main(){
-	pessoa := new(Pessoa)
-	pessoa.nome = "Augusto"
-	pessoa.idade = 23
-	pessoa.salario = 1000
+	/* ////////////
+	   // Arrays //
+	*/ ////////////
 
-	pessoa.addSalary(10)
-	fmt.Println(pessoa.salario)
+	// Alternativa 1
+	var salarios1 [5]int // Array de inteiros com cinco posições
+	salarios1[0] = 1000
+	salarios1[1] = 1500
+	// range retorna o indice e o valor.
+	// _ é usado quando não queremos guardar o valor retornado.
+	for _, salario := range salarios1{
+		fmt.Println(salario)
+	}
+	fmt.Println()
+
+	// Alternativa 2
+	salarios2 := make([]int, 5) // Array de inteiros com cinco posições
+	salarios2[0] = 2000
+	salarios2[1] = 3000
+	salarios2[4] = 6000
+	for _, salario := range salarios2{
+		fmt.Println(salario)
+	}
+	fmt.Println()
+
+	/* ////////////
+	   // Slices //
+	*/ ////////////
+	
+	// Valores pré-definidos
+	salarios3 := []int{1000, 2000, 3000, 4000, 5000}
+	for i := 0; i < len(salarios3); i++{
+		salarios3[i] += 100 + i
+	}
+	for _, salario := range salarios3{
+		fmt.Println(salario)
+	}
+	fmt.Println()
+
+	// Slice vazio
+	salarios4 := []int{}
+	for i := 0; i < 5; i++{
+		salarios4 = append(salarios4, 100 + i)
+	}
+	for _, salario := range salarios4{
+		fmt.Println(salario)
+	}
 
 }
 
-func setName(name string) {
-	fmt.Println("Hello", name)
-}
