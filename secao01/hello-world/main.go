046c961208177bfb639d47592a61cf54cb963115
Augusto-Fadanelli <augustofadanelli@protonmail.com>
2024-05-10 16:34:51 -0300

    Aula 14: trabalhando com packages

--- secao01/hello-world/main.go
+++ secao01/hello-world/main.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"gohello/ping"
 )
 
 func main(){
-	salFuncs := make(map[string] int)
-	salFuncs["Augusto"] = 10
-	salFuncs["Fadanelli"] = 20
-
-	sal, exists := salFuncs["Fadanelli"]
-	fmt.Println(sal, exists)
-	qtdSal := len(salFuncs)
-	fmt.Println(qtdSal)
+	ping.Pong()
 }
 
